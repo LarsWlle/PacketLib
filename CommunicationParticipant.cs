127c82dc1091ec179d481dc9fe263332ae2e4f9f
Lars Wille <larswille2007@gmail.com>
2026-06-02 17:15:52 +0200

    moved common logic to communication participant

---
+++ CommunicationParticipant.cs
@@ -0,0 +1,41 @@
+using PacketLib.Clients;
+using PacketLib.Packets;
+
+namespace PacketLib;
+
+public abstract class CommunicationParticipant {
+    private readonly List<IPacketHandlerLayer> _handleLayers = [];
+    private readonly List<IPacketPackageLayer> _packageLayers = [];
+
+    public IReadOnlyList<IPacketHandlerLayer> HandleLayers => this._handleLayers.AsReadOnly();
+
+    private readonly List<InboundPacket> _inboundPackets = [];
+
+    public void AddPackageHandler(IPacketPackageLayer handler) {
+        this._packageLayers.Add(handler);
+        Logger.Debug($"Added new packet package layer with priority = {handler.GetPriority()}");
+    }
+
+    public void AddHandlerLayer(IPacketHandlerLayer handler) {
+        this._handleLayers.Add(handler);
+        Logger.Debug($"Added new packet handler layer with priority = {handler.GetPriority()}");
+    }
+
+    public void RegisterInboundPacket(InboundPacket packet) {
+        if (this._inboundPackets.Contains(packet)) {
+            Logger.Fatal($"Packet with id {packet.GetId()} already exists");
+        }
+
+        this._inboundPackets.Add(packet);
+    }
+
+    internal InboundPacket? GetInboundPacket(ushort id) {
+        try {
+            return this._inboundPackets.First(p => p.GetId() == id);
+        }
+        catch (InvalidOperationException ex) {
+            Logger.Fatal($"Could not find packet with id {id}");
+            return null;
+        }
+    }
+}
--- Server.cs
+++ Server.cs
@@ -6,45 +6,11 @@ using PacketLib.Packets;
 
 namespace PacketLib;
 
-public class Server(Func<TcpClient, int, Server, BaseClient> clientFactory) {
+public class Server(Func<TcpClient, int, Server, BaseClient> clientFactory) : CommunicationParticipant {
     public Config Config { get; private set; } = new();
     private List<BaseClient> _clients = [];
-    private readonly List<IPacketHandlerLayer> _handleLayers = [];
-    private readonly List<IPacketPackageLayer> _packageLayers = [];
-
-    public IReadOnlyList<IPacketHandlerLayer> HandleLayers => this._handleLayers.AsReadOnly();
-
-    private readonly List<InboundPacket> _inboundPackets = [];
 
     private int _lastClientId = 0;
-
-    public void AddPackageHandler(IPacketPackageLayer handler) {
-        this._packageLayers.Add(handler);
-        Logger.Debug($"Added new packet package layer with priority = {handler.GetPriority()}");
-    }
-
-    public void AddHandlerLayer(IPacketHandlerLayer handler) {
-        this._handleLayers.Add(handler);
-        Logger.Debug($"Added new packet handler layer with priority = {handler.GetPriority()}");
-    }
-
-    public void RegisterInboundPacket(InboundPacket packet) {
-        if (this._inboundPackets.Contains(packet)) {
-            Logger.Fatal($"Packet with id {packet.GetId()} already exists");
-        }
-
-        this._inboundPackets.Add(packet);
-    }
-
-    internal InboundPacket? GetInboundPacket(ushort id) {
-        try {
-            return this._inboundPackets.First(p => p.GetId() == id);
-        }
-        catch (InvalidOperationException ex) {
-            Logger.Fatal($"Could not find packet with id {id}");
-            return null;
-        }
-    }
 
     public void Start() {
         TcpListener listener = new(IPAddress.Any, this.Config.Port) {
