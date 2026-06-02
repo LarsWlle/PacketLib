439a018003db1baeab5eaf5d29f32d7d348cbbe1
Lars Wille <larswille2007@gmail.com>
2026-06-02 18:06:47 +0200

    removed server test

--- Program.cs
+++ Program.cs
@@ -3,23 +3,13 @@ using System.Runtime.CompilerServices;
 using PacketLib.Clients;
 using PacketLib.Clients.Impl.Inbounds;
 using PacketLib.Clients.Impl.Outbounds;
-using PacketLib.Configuration;
 
 namespace PacketLib;
 
 class Program {
     static void Main(string[] args) {
-        Program prog = new Program();
+        Program prog = new();
         Task.Run(prog.TestClient);
-        prog.TestServer();
-    }
-
-    private void TestServer() {
-        Server server = new(((tcp, id, s) => new Client(tcp, id, s)));
-
-        server.AddHandlerLayer(new EncryptionHandleLayer());
-        server.AddPackageHandler(new EncryptionPackageLayer());
-        server.Start();
     }
 
     private void TestClient() {
