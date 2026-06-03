a4861a9f46ec11aab3957190486fcb033eea9537
Lars Wille <larswille2007@gmail.com>
2026-06-03 17:13:07 +0200

    added send method inside the communication client

--- CommunicationClient.cs
+++ CommunicationClient.cs
@@ -1,4 +1,7 @@
 using System.Net.Sockets;
+using PacketLib.Clients;
+using PacketLib.Packets;
+using PacketLib.Utils;
 
 namespace PacketLib;
 
@@ -7,5 +10,21 @@ public class CommunicationClient : CommunicationParticipant {
 
     public void Connect(string endpoint, int port) {
         this._client = new(endpoint, port);
+    }
+
+    public void Send(OutboundPacket packet) {
+        NetworkStream stream = this._client.GetStream();
+        byte[] data = packet.Package();
+
+        IReadOnlyList<INetworkLayer> sorted = this.PackageLayers.OrderBy(p => p.GetPriority()).ToList();
+        byte[] result = sorted.Aggregate(data, (current, layer) => layer.Handle(current));
+
+        byte[] final = [
+            ..(result.Length + 6).ToByteArray(), // + 6 = + 4 (length) + 2 (packet id)
+            ..packet.GetId().ToByteArray(),
+            ..result
+        ];
+
+        stream.Write(final, 0, final.Length);
     }
 }
