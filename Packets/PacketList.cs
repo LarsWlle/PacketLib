046d7b80c41b2e67b45ff44f8c3be6401a273462
Lars Wille <larswille2007@gmail.com>
2026-06-06 14:53:41 +0200

    changed add method inside packet list

--- Packets/PacketList.cs
+++ Packets/PacketList.cs
@@ -3,13 +3,15 @@ namespace PacketLib.Packets;
 public class PacketList {
     private Dictionary<int, InboundPacket> _packets = [];
 
-    public void Add(InboundPacket packet) {
+    public PacketList Add(InboundPacket packet) {
         if (this._packets.ContainsKey(packet.GetId())) {
             Logger.Fatal($"Packet with id {packet.GetId()} already exists");
         }
 
         this._packets[packet.GetId()] = packet;
         Logger.Debug($"Registered new packet[id={packet.GetId()},name\"{packet.GetType().Name}\"]");
+
+        return this;
     }
 
     public InboundPacket? Get(int id) {
