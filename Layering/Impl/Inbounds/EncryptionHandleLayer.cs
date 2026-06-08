1bc0f962db9f7e8fe588929614c83bba621115d1
Lars Wille <larswille2007@gmail.com>
2026-06-08 16:09:05 +0200

    removed old methods no longer used

--- Layering/Impl/Inbounds/EncryptionHandleLayer.cs
+++ Layering/Impl/Inbounds/EncryptionHandleLayer.cs
@@ -1,7 +1,5 @@
 namespace PacketLib.Layering.Impl.Inbounds;
 
 public class EncryptionHandleLayer : INetworkLayer {
-    public int GetPriority() => int.MinValue;
-
     public byte[] Handle(byte[] data, AbstractClient client) => client.Encryption.IsHandshakeComplete ? client.Encryption.Decrypt(data) : data;
 }
--- Layering/Impl/Outbounds/EncryptionPackageLayer.cs
+++ Layering/Impl/Outbounds/EncryptionPackageLayer.cs
@@ -1,7 +1,5 @@
 namespace PacketLib.Layering.Impl.Outbounds;
 
 public class EncryptionPackageLayer : INetworkLayer {
-    public int GetPriority() => int.MaxValue;
-
     public byte[] Handle(byte[] packet, AbstractClient client) => client.Encryption.IsHandshakeComplete ? client.Encryption.Encrypt(packet) : packet;
 }
